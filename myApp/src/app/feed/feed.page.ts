df31a9a45d80e8cb261e17bd885cc9c0763fcfe9
Rony <journal.indoor@gmail.com>
2021-01-21 13:53:10 -0300

    [newdev] Criando funções

--- myApp/src/app/feed/feed.page.ts
+++ myApp/src/app/feed/feed.page.ts
@@ -11,7 +11,12 @@ export class FeedPage implements OnInit {
 
   constructor() { }
 
+  public somaDoisNumeros(): void{
+    alert(5 + 10);
+  }
+
   ngOnInit() {
+    this.somaDoisNumeros();
   }
 
 }
